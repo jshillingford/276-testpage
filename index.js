a83b8126185869efd407d84ed715d04acda3bbb3
Justin Shillingford <Justin@Justins-MacBook-Bro.local>
2019-06-12 23:42:59 -0700

    test db con

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+const PORT = process.env.PORT || 5000 //set to port given in environment var or port 5000 if not found
+
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+  user: 'postgres',
+  password: 'root',
+  host: 'localhost',
+  database: 'postgres',
+  ssl: false
 });
-const PORT = process.env.PORT || 5000
 
-//const app = express() , then use 'app' as a object below
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index')) //get from root of app pages/index
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => res.send(showTimes()))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+//do app.post to get request from my html file
+app.get('/', (req, res) => res.render('pages/index')) //get from root of app pages/index (the => represents a funciton)
+app.get('/cool', (req, res) => res.send(cool()))
+app.get('/times', (req, res) => res.send(showTimes()))
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 showTimes = () => {
   let result = ''
@@ -38,3 +42,9 @@ showTimes = () => {
   }
   return result;
 }
+
+
+//to add delete functionality:
+//in html,  add form with button
+// here, add app.post(/deleteuser) ... => ... res.redirect('/')
+// then, app.get(/delete) beneath, or what he has in the file
